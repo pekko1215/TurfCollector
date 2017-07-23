3948b74f7d7d9b2b66eed26632bae0edbeb5a6f8
pekko1215 <pekko1215@yahoo.co.jp>
2017-07-23 11:08:22 +0900

    おわり

---
+++ TurfCollector.js
@@ -0,0 +1,12 @@
+module.exports = {
+    get:function(username,callback){
+        var xRay = require("x-ray")();
+        console.log("https://github.com/users/"+username+"/contributions")
+        xRay("https://github.com/users/"+username+"/contributions","rect",[
+            {
+                "level":"@data-count",
+                "date":"@data-date"
+            }
+        ])(callback)
+    }
+}
---
+++ sample.js
@@ -0,0 +1,5 @@
+var turf = require("./TurfCollector");
+
+turf.get("pekko1215",function(e,data){
+    console.log(data)
+})
